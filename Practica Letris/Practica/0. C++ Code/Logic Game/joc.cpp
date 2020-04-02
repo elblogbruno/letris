224bd54c84a79f07941624da9b0278dbdcc8e629
elblogbruno <glassearproject@gmail.com>
2020-04-02 13:21:00 +0200

    Primera part practica

--- Practica Letris/Practica/0. C++ Code/Logic Game/joc.cpp
+++ Practica Letris/Practica/0. C++ Code/Logic Game/joc.cpp
@@ -3,8 +3,9 @@
 #include "Button.h"
 #include <iostream>
 using namespace std;
-int m_estatActual = 0;
-int m_llenguatjeActual = 0;
+
+
+
 //-----------------------------------------
 // joc: porta el control de tot el joc
 //-----------------------------------------
@@ -27,31 +28,42 @@ void joc()
 
 	//-----TODO 1
 	//Crear un timer i mostrar per pantalla el temps passat
-	
+
 	NFont fontBlanca("data/Fonts/FreeSans.ttf", 30, NFont::Color(255, 255, 255, 255));
 	NFont fontNegra("data/Fonts/FreeSans.ttf", 30, NFont::Color(0, 0, 0, 255));
-	
+
 	//-----TODO 1
 
 
 	//-----TODO 2
 	//-Pintar una matriu de 4x4 amb el sprite de la lletra 'a' de letris
 	//-Pintar un sprite de bot� i cada cop que es clica pintar o deixar de pintar el bot�
+
+	//MAIN_MENU VARIABLES
 	bool drawCatala = false;
 	bool drawCastella = false;
 	bool drawAngles = false;
 	bool startPresionat = false;
+	//PLAYING VARIABLES
 	bool drawMatrix = false;
 	bool pausarContador = false;
 	bool check = false;
 	bool eliminarMatriu = false;
 	bool drawTimer = true;
+	//GAME OVER VARIABLES
 	bool esGameOver = false;
-	//Variables per els diferents timers.
-	float timer = 0.0f;
-	float timerBoto = 0.0f;
-	float timerGameOver = 0.0f;
 
+
+	// Variables per els diferents timers.
+	float timerPartida = 0.0f;
+	float timerBoto = 0.0f; //aquest timer estava pensat per la animacio del boto quan es fa click en ell.
+	float timerGameOver = 0.0f;
+	//
+	int estatMatriu = 0; //Variable que guarda l'estat de la matriu de lletres MOSTRA_MATRIU = 0 i AMAGAR_MATRIU = 1;
+	int m_estatActual = 0; //Variable que guarda l'estat del menu de lletres MAIN_MENU = 0, PLAYING = 1,PAUSE = 2 i GAME_OVER = 3;
+	int m_llenguatjeActual = 0; //Variable que guarda el llenguatje actual del menu CATALA = 0, CASTELLA = 1 i ANGLES = 2;
+	//
+	//sprite dels radio-buttons
 	Sprite botoEscollirIdiomaNoEscollit("./data/GUI/Checkbox_Disabled.png");
 	Sprite botoEscollirIdiomaEscollit("./data/GUI/Checkbox_Enabled.png");
 
@@ -70,10 +82,12 @@ void joc()
 	Sprite botoCheckPresionat("./data/GUI/Button_Pressed.png");
 	Sprite botoCheckNoPresionat("./data/GUI/Button_Normal.png");
 
+	//fons de cada menu
 	Sprite fonsMenuInicial("./data/GUI/MainMenu_BG.png");
 	Sprite fonsPlaying("./data/GUI/background_play.png");
 	Sprite fonsFinalPartida("./data/GUI/GameOver.png");
-	//-----TODO 2
+
+	//sprite de les lletres
 	Sprite lletraA("./data/Letters/white_0.png");
 	Sprite lletraB("./data/Letters/white_1.png");
 	Sprite lletraC("./data/Letters/white_2.png");
@@ -83,32 +97,33 @@ void joc()
 	Sprite lletraG("./data/Letters/white_6.png");
 	Sprite lletraH("./data/Letters/white_7.png");
 
-	Sprite lletres[8] = { lletraA,lletraB,lletraC,lletraD,lletraE,lletraF,lletraG,lletraH};
+	Sprite lletres[8] = { lletraA,lletraB,lletraC,lletraD,lletraE,lletraF,lletraG,lletraH };
+
 	/*string idiomesDisponiblesPosicioTitol[3] = { "Catala", "Castella","Angles" };
 	int idiomesDisponiblesPosicio[3]= { 100,130,160 };
 	Button idiomesDisponiblesButtons[3];*/
+	/*for (int i = 0; i < 3; i++)
+	{
+		idiomesDisponiblesButtons[i] = Button(100, idiomesDisponiblesPosicio[i], idiomesDisponiblesPosicioTitol[i]);
+	}*/
 
-	//Buttons
-	Button botoCatala = Button(100, 120, "Cat");
-	Button botoCastella = Button(100, 170, "Cast");
-	Button botoAngles = Button(100, 220, "Ang");
-	Button botoStart = Button(150,725, "Start");
+	//Botons creats amb la classe Button.
+	Button botoCatala = Button(100, 120, "Catala");
+	Button botoCastella = Button(100, 170, "Castella");
+	Button botoAngles = Button(100, 220, "Angles");
+	Button botoStart = Button(150, 725, "START");
 	Button botoPausaPlayContador = Button(375, 40, "Contador");
 	Button botoEliminar = Button(375, 760, "Eliminar");
-	Button botoCheck = Button(160, 760, "Check");
+	Button botoCheck = Button(160, 760, "CHECK");
 
-	bool potDibuixar = true;
+
 	Uint64 NOW = SDL_GetPerformanceCounter();
 	Uint64 LAST = 0;
 	double deltaTime = 0;
 
 
-	/*for (int i = 0; i < 3; i++)
-	{
-		idiomesDisponiblesButtons[i] = Button(100, idiomesDisponiblesPosicio[i], idiomesDisponiblesPosicioTitol[i]);
-	}*/
-	int currentLanguageButton = 0;
-	int estatMatriu = 0;
+
+
 	do
 	{
 		LAST = NOW;
@@ -124,130 +139,126 @@ void joc()
 		int mousePosX = Mouse_getX();
 		int mousePosY = Mouse_getY();
 
-		
+
 		switch (m_estatActual)
 		{
-			case MAIN_MENU:
-				//Dibuixa  el fons de pantalla.
-				fonsMenuInicial.draw(0,0);
-				//Dibuixa el boto sense escollir per default
-				botoEscollirIdiomaNoEscollit.draw(botoCatala.getButtonX(), botoCatala.getButtonY());
-				botoEscollirIdiomaNoEscollit.draw(botoCastella.getButtonX(), botoCastella.getButtonY());
-				botoEscollirIdiomaNoEscollit.draw(botoAngles.getButtonX(), botoAngles.getButtonY());
-				//Dibuixa el text a costat de cada boto. TODO : En lloc de hardcoded, demanar com escriure char o string en NFONT
-				fontBlanca.draw(150, botoCatala.getButtonY()- 6, NFont::Scale(0.85f), "Catala");
-				fontBlanca.draw(150, botoCastella.getButtonY() - 6, NFont::Scale(0.85f), "Castella");
-				fontBlanca.draw(150,botoAngles.getButtonY() - 6, NFont::Scale(0.85f), "Angles");
-				drawCatala = botoCatala.updateButtonRadioButton(mousePosX, mousePosY, moousePressed, m_llenguatjeActual);
-				drawCastella = botoCastella.updateButtonRadioButton(mousePosX, mousePosY, moousePressed, m_llenguatjeActual);
-				drawAngles = botoAngles.updateButtonRadioButton(mousePosX, mousePosY, moousePressed, m_llenguatjeActual);
-				switch (m_llenguatjeActual)
-				{
-					case CATALA:
-						botoEscollirIdiomaEscollit.draw(botoCatala.getButtonX(), botoCatala.getButtonY());
-						break;
-					case CASTELLA:
-						botoEscollirIdiomaEscollit.draw(botoCastella.getButtonX(), botoCastella.getButtonY());
-						break;
-					case ANGLES:
-						botoEscollirIdiomaEscollit.draw(botoAngles.getButtonX(), botoAngles.getButtonY());
-						
-						break;
-				default:
-					break;
-				}
-				
-				botoStartNoPresionar.draw(botoStart.getButtonX(), botoStart.getButtonY());
-				fontBlanca.draw(botoStart.getButtonX()+50, botoStart.getButtonY()+20, NFont::Scale(0.85f), "START");
-				startPresionat = botoStart.updateButton(mousePosX, mousePosY, moousePressed);
-				if (startPresionat) {
-					botoStartPresionat.draw(botoStart.getButtonX(), botoStart.getButtonY());
-					m_estatActual = PLAYING;
-				}
-				break;
-			case PLAYING:
-				fonsPlaying.draw(0, 0);
-				timeBar.draw(120,40);
-				
-				pausarContador = botoPausaPlayContador.updateButton(mousePosX, mousePosY, moousePressed);
-				if (!pausarContador) {
-					timer += deltaTime; // Va sumant els segons
-					botoContadorPausa.draw(botoPausaPlayContador.getButtonX(), botoPausaPlayContador.getButtonY());
-				}
-				else {
-					botoContadorPlay.draw(botoPausaPlayContador.getButtonX(), botoPausaPlayContador.getButtonY());			
-				}
-				
-				if (drawTimer) {
-					fontNegra.draw(200, 50, NFont::Scale(0.85f), "%d", (int)timer);
-				}
-				if (timer > 10) {
-					drawTimer = false;
-					m_estatActual = GAME_OVER; //Han passat 10 segons, llavors canvia a la pantalla de GameOver
-					esGameOver = false;
-				}
+		case MAIN_MENU:
 
-				
-				check = botoCheck.updateButton(mousePosX, mousePosY, moousePressed);
-
-				eliminarMatriu = botoEliminar.updateButton(mousePosX, mousePosY, moousePressed);
-				
-				if (eliminarMatriu) {
-					botoEliminarNoPresionat.draw(botoEliminar.getButtonX(), botoEliminar.getButtonY());
-					estatMatriu = AMAGAR_MATRIU;
-				}
-				else {
-					botoEliminarPresionat.draw(botoEliminar.getButtonX(), botoEliminar.getButtonY());
-				}
-				if (check) {
-					botoCheckNoPresionat.draw(botoCheck.getButtonX(), botoCheck.getButtonY());
-					estatMatriu = MOSTRA_MATRIU;
-				}
-				else {
-					botoCheckPresionat.draw(botoCheck.getButtonX(), botoCheck.getButtonY());
-				}
-				fontBlanca.draw(botoCheck.getButtonX() + 10, botoCheck.getButtonY(), NFont::Scale(1.5f), "CHECK");
-				switch (estatMatriu)
-				{
-					case MOSTRA_MATRIU:
-						drawMatrix = true;
-						break;
-					case AMAGAR_MATRIU:
-						drawMatrix = false;
-						break;
-				}
-				
-				//cout << "CHECK: " << check << "Eliminar Matriu: " << eliminarMatriu << "Draw Matriu: " << drawMatrix;
-				if (drawMatrix) {
-					for (int i = 0; i < 10; i++) {
-						for (int j = 0; j < 8; j++) {
-							int posX = 10 + (j*TILE_WIDTH + 30);
-							int posY = 100 + (i*TILE_WIDTH + 50);
-							lletres[j].setScale(1);
-							lletres[j].draw(posX, posY);
-						}
-					}
-				}
+			//Dibuixa  el fons de pantalla.
+			fonsMenuInicial.draw(0, 0);
+			//Dibuixa el boto sense escollir per default
+			botoEscollirIdiomaNoEscollit.draw(botoCatala.getButtonX(), botoCatala.getButtonY());
+			botoEscollirIdiomaNoEscollit.draw(botoCastella.getButtonX(), botoCastella.getButtonY());
+			botoEscollirIdiomaNoEscollit.draw(botoAngles.getButtonX(), botoAngles.getButtonY());
+			//Dibuixa el text a costat de cada boto. TODO : En lloc de hardcoded, demanar com escriure char o string en NFONT
+			fontBlanca.draw(150, botoCatala.getButtonY() - 6, NFont::Scale(0.85f), "Catala");
+			fontBlanca.draw(150, botoCastella.getButtonY() - 6, NFont::Scale(0.85f), "Castella");
+			fontBlanca.draw(150, botoAngles.getButtonY() - 6, NFont::Scale(0.85f), "Angles");
+			//
+			drawCatala = botoCatala.updateButtonRadioButton(mousePosX, mousePosY, moousePressed, m_llenguatjeActual);
+			drawCastella = botoCastella.updateButtonRadioButton(mousePosX, mousePosY, moousePressed, m_llenguatjeActual);
+			drawAngles = botoAngles.updateButtonRadioButton(mousePosX, mousePosY, moousePressed, m_llenguatjeActual);
+			switch (m_llenguatjeActual)
+			{
+			case CATALA:
+				botoEscollirIdiomaEscollit.draw(botoCatala.getButtonX(), botoCatala.getButtonY());
 				break;
-			case GAME_OVER:
-				//NO SABEM PER QUINA RAO, SI FEM QUE COMENÇI EL JOC AMB LA PANTALLA DE GAME OVER, FUNCIONA CORRECTAMENT, A DIFERENCIA DE INICIALITZARLA QUAN TOCA
-				// ON TROBEM QUE LA PANTALLA TORNA AL MAIN MENU PER UNA MILESIMA DE SEGON I RETORNA A GAME OVER.
-				fonsFinalPartida.draw(0, 0);
-				if (!esGameOver) {
-					timerGameOver += deltaTime;
-					//fontNegra.draw(200, 50, NFont::Scale(0.85f), "%d", (int)timerGameOver);
-				}	
-				if (timerGameOver > 5.0f) {
-					fonsFinalPartida.draw(800, 800);
-					//fontNegra.draw(200, 50, NFont::Scale(0.85f), "TIME'S OVER");
-					m_estatActual = MAIN_MENU;
-					esGameOver = true;
-					timerGameOver = 0;
-					deltaTime = 0;
-				}
+			case CASTELLA:
+				botoEscollirIdiomaEscollit.draw(botoCastella.getButtonX(), botoCastella.getButtonY());
+				break;
+			case ANGLES:
+				botoEscollirIdiomaEscollit.draw(botoAngles.getButtonX(), botoAngles.getButtonY());
 				break;
 			default:
 				break;
+			}
+			botoStartNoPresionar.draw(botoStart.getButtonX(), botoStart.getButtonY());
+			fontBlanca.draw(botoStart.getButtonX() + 50, botoStart.getButtonY() + 20, NFont::Scale(0.85f), "START");
+			startPresionat = botoStart.updateButton(mousePosX, mousePosY, moousePressed);
+			if (startPresionat) {
+				botoStartPresionat.draw(botoStart.getButtonX(), botoStart.getButtonY());
+				m_estatActual = PLAYING;
+			}
+			break;
+		case PLAYING:
+			fonsPlaying.draw(0, 0);
+			timeBar.draw(120, 40);
+
+			pausarContador = botoPausaPlayContador.updateButton(mousePosX, mousePosY, moousePressed);
+			if (!pausarContador) {
+				timerPartida += deltaTime; // Va sumant els segons
+				botoContadorPausa.draw(botoPausaPlayContador.getButtonX(), botoPausaPlayContador.getButtonY());
+			}
+			else {
+				botoContadorPlay.draw(botoPausaPlayContador.getButtonX(), botoPausaPlayContador.getButtonY());
+			}
+
+			if (drawTimer) {
+				fontNegra.draw(200, 50, NFont::Scale(0.85f), "%d", (int)timerPartida);
+			}
+			if (timerPartida > 10) {
+				drawTimer = false;
+				m_estatActual = GAME_OVER; //Han passat 10 segons, llavors canvia a la pantalla de GameOver
+				esGameOver = true;
+			}
+
+			check = botoCheck.updateButton(mousePosX, mousePosY, moousePressed);
+
+			eliminarMatriu = botoEliminar.updateButton(mousePosX, mousePosY, moousePressed);
+
+			if (eliminarMatriu) {
+				botoEliminarNoPresionat.draw(botoEliminar.getButtonX(), botoEliminar.getButtonY());
+				estatMatriu = AMAGAR_MATRIU;
+			}
+			else {
+				botoEliminarPresionat.draw(botoEliminar.getButtonX(), botoEliminar.getButtonY());
+			}
+			if (check) {
+				botoCheckNoPresionat.draw(botoCheck.getButtonX(), botoCheck.getButtonY());
+				estatMatriu = MOSTRA_MATRIU;
+			}
+			else {
+				botoCheckPresionat.draw(botoCheck.getButtonX(), botoCheck.getButtonY());
+			}
+			fontBlanca.draw(botoCheck.getButtonX() + 10, botoCheck.getButtonY(), NFont::Scale(1.5f), "CHECK");
+			switch (estatMatriu)
+			{
+			case MOSTRA_MATRIU:
+				drawMatrix = true;
+				break;
+			case AMAGAR_MATRIU:
+				drawMatrix = false;
+				break;
+			}
+
+			//cout << "CHECK: " << check << "Eliminar Matriu: " << eliminarMatriu << "Draw Matriu: " << drawMatrix;
+			if (drawMatrix) {
+				for (int i = 0; i < 10; i++) {
+					for (int j = 0; j < 8; j++) {
+						int posX = 10 + (j*TILE_WIDTH + 30);
+						int posY = 100 + (i*TILE_WIDTH + 50);
+						lletres[j].setScale(1);
+						lletres[j].draw(posX, posY);
+					}
+				}
+			}
+			break;
+		case GAME_OVER:
+			//NO SABEM PER QUINA RAO, SI FEM QUE COMENÇI EL JOC AMB LA PANTALLA DE GAME OVER, FUNCIONA CORRECTAMENT, A DIFERENCIA DE INICIALITZARLA QUAN TOCA
+			// ON TROBEM QUE LA PANTALLA TORNA AL MAIN MENU PER UNA MILESIMA DE SEGON I RETORNA A GAME OVER.
+			fonsFinalPartida.draw(0, 0);
+			if (esGameOver) {
+				timerGameOver += deltaTime;
+				//fontNegra.draw(200, 50, NFont::Scale(0.85f), "%d", (int)timerGameOver);
+			}
+			if (timerGameOver > 5.0f) {
+				esGameOver = false;
+				timerGameOver = 0;
+				m_estatActual = MAIN_MENU;
+			}
+			break;
+		default:
+			break;
 		}
 
 
